0ff8dbdc3dde9fb8facb57666b7a9069d4cef9ec
Luugallego17 <luna.gallego@ucb.edu.bo>
2025-04-07 12:33:24 +0000

    Ejercicio 4 corregido :(

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -7,14 +7,20 @@ int main () {
     cout << "ingresa un numero entero positivo" << endl;
     cin >> n;
     
+    while (n < 0 || n >= 15){
   if (n < 0){
       cout << "no se puede calcular con numeros negativos" << endl;
-  } else {
+  } else if (n >= 15){
+      cout << "el numero es muy grande, no se puede calcular " << endl;
+  }
+     cout << "ingresa un numero entero positivo" << endl;
+    cin >> n;    
+    }
+  
       for (int i = 1; i <= n; i++){
           r*=i;
-      }
   }
-     cout << "el factorial de " << n << " es " << r << endl;
-     
+  cout << "el factorial de " << n << " es " << r << endl;
+  
     return 0;
 }
